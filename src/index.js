83bf06e402d9d20a1c570e4e0376c539bd7b3bf8
Sanje Divakaran <sanje.divakaran@gmail.com>
2021-04-13 12:03:46 -0400

    Initial Load

---
+++ src/index.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
+import 'semantic-ui-css/semantic.min.css';
+
+class App extends React.Component {
+    state = { lat: null, errorMessage: '' };
+
+    componentDidMount() {
+        //Get the users position
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => {
+                // To update a state, use setState***
+                this.setState({ lat: position.coords.latitude });
+            },
+            (err) => {
+                this.setState({ errorMessage: err.message });
+            }
+        ) 
+    }
+
+    renderContent() {
+        if (this.state.errorMessage && !this.state.lat){
+            return <div>Error: { this.state.errorMessage }</div>
+        }
+        if (!this.state.errorMessage && this.state.lat){
+            return <SeasonDisplay lat={ this.state.lat }/>
+        }
+        
+        return (
+            <Spinner message="Please accept location request" />
+        );
+    }
+
+    // React says we have to define render
+    render() {
+        return (
+            <div className="border-red">
+                {this.renderContent()}
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(
+    <App/>, document.querySelector('#root')
+)
